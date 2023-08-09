2f612a32d7ef5afd1a83b9029940e83a0ed12f36
Taekhwan Han <hth3396@gmail.com>
2023-08-10 00:55:45 +0900

    feat: #1 현재 위치 오버레이로 표기

--- FREEWAY/FREEWAY/Presentation/MapsView/MapsViewController.swift
+++ FREEWAY/FREEWAY/Presentation/MapsView/MapsViewController.swift
@@ -7,27 +7,82 @@
 
 import UIKit
 import NMapsMap
+import Then
+import SnapKit
+import CoreLocation
 
 class MapsViewController: UIViewController {
+    
+    private var location = CLLocationManager()
+    private let locationMarker = NMFMarker()
+    private var locationOverlay: NMFLocationOverlay?
     
     private lazy var mapsView = NMFMapView().then {
         $0.allowsZooming = true
         $0.logoInteractionEnabled = false
         $0.allowsScrolling = true
+        locationOverlay = $0.locationOverlay
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         setupLayout()
+        configureLocation()
     }
 }
 
 private extension MapsViewController {
+    
+    func configureLocation() {
+        location.delegate = self
+        location.desiredAccuracy = kCLLocationAccuracyBest
+        location.requestWhenInUseAuthorization()
+    }
+    
+    func setLocation() {
+        let latitude = location.location?.coordinate.latitude ?? 0
+        let longitude = location.location?.coordinate.longitude ?? 0
+        
+        if CLLocationManager.locationServicesEnabled() {
+            //TODO: 테스트 구문
+            print("위치 서비스 On 상태")
+            location.startUpdatingLocation()
+            print(latitude, longitude)
+            
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: latitude, lng: longitude), zoomTo: 15)
+            cameraUpdate.animation = .easeIn
+            mapsView.moveCamera(cameraUpdate)
+            guard let locationOverlay = locationOverlay else { return }
+                    locationOverlay.hidden = false
+                    locationOverlay.location = NMGLatLng(lat: latitude, lng: longitude)
+                    locationOverlay.circleOutlineWidth = 10
+            
+        } else {
+            //TODO: 테스트 구문
+            print("위치 서비스 Off 상태")
+        }
+    }
+    
     func setupLayout() {
         self.view.addSubview(mapsView)
         mapsView.snp.makeConstraints { make in
             make.top.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+}
+
+extension MapsViewController: CLLocationManagerDelegate {
+    //TODO: 현재 위치 테스트 구문
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            setLocation()
+        case .restricted, .notDetermined:
+            print("GPS 권한 설정되지 않음")
+        case .denied:
+            print("GPS 권한 요청 거부됨")
+        default:
+            print("GPS: Default")
         }
     }
 }
