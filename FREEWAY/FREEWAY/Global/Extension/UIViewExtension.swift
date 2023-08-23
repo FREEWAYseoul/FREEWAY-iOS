67400c60b93905656b7994c8b538365febbbf4fa
Taekhwan Han <hth3396@gmail.com>
2023-08-23 22:27:28 +0900

    feat: #1 UIViewToUIImage 로직 변경

--- FREEWAY/FREEWAY/Global/Extension/UIViewExtension.swift
+++ FREEWAY/FREEWAY/Global/Extension/UIViewExtension.swift
@@ -9,9 +9,9 @@ import UIKit
 
 extension UIView {
     func toImage() -> UIImage? {
-        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        let renderer = UIGraphicsImageRenderer (bounds: bounds)
         return renderer.image { rendererContext in
-            layer.render(in: rendererContext.cgContext)
+            drawHierarchy (in: bounds, afterScreenUpdates: true)
         }
     }
 }
