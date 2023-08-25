9fadb02f16d4ca2c087ea557aea572a56c79fb6d
Taekhwan Han <hth3396@gmail.com>
2023-08-25 19:10:01 +0900

    feat: #6 검색결과가 없을 시에 뷰 구현

---
+++ FREEWAY/FREEWAY/Presentation/SearchView/Components/EmptySearchView.swift
@@ -0,0 +1,56 @@
+//
+//  EmptySearchView.swift
+//  FREEWAY
+//
+//  Created by 한택환 on 2023/08/25.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+final class EmptySearchView: UIView {
+    //추후에 TextField와 데이터 바인딩 필요
+    var searchText: String = "강남"
+    
+    private lazy var emptySearchTitleLabel = UILabel().then {
+        $0.textColor = Pallete.customGray.color
+        $0.text = "\"\(searchText)\" 검색 결과가 없습니다."
+        $0.font = UIFont(name: "Pretendard-SemiBold", size: 20)
+    }
+    
+    private let emptySearchImage = UIImageView().then {
+        $0.image = UIImage(named: "error")
+        $0.contentMode = .scaleAspectFill
+        $0.tintColor = Pallete.customGray.color
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = Pallete.backgroundGray.color
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension EmptySearchView {
+    
+    func setupLayout() {
+        self.addSubview(emptySearchImage)
+        emptySearchImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(147.4)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(60)
+        }
+        
+        self.addSubview(emptySearchTitleLabel)
+        emptySearchTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(emptySearchImage.snp.bottom).offset(19.6)
+            make.centerX.equalToSuperview()
+        }
+    }
+}
+
