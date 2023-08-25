4e1c347448574eab02f05885ef141b4b52535a1e
Taekhwan Han <hth3396@gmail.com>
2023-08-26 01:07:13 +0900

    feat: #2 HomeViewController UI 구현

--- FREEWAY/FREEWAY/Presentation/HomeView/HomeViewController.swift
+++ FREEWAY/FREEWAY/Presentation/HomeView/HomeViewController.swift
@@ -11,4 +11,52 @@ import Then
 
 final class HomeViewController: UIViewController {
     
+    private let homeTitle = HomeTitleView()
+    
+    private let textField = HomeSearchTextfieldView()
+    
+    private let recentSearchView = RecentSearchView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupLayout()
+    }
+    
+    private func safeAreaTopInset() -> CGFloat? {
+        if #available(iOS 15.0, *) {
+            let topArea = UIApplication.shared.windows.first?.safeAreaInsets.top
+            return topArea
+        } else {
+            return nil
+        }
+    }
+}
+
+private extension HomeViewController {
+    
+    func setupLayout() {
+        view.addSubview(homeTitle)
+        homeTitle.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset((safeAreaTopInset() ?? 50) + 162)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        view.addSubview(textField)
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(homeTitle.snp.bottom).offset(41)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(72)
+        }
+        
+        view.addSubview(recentSearchView)
+        recentSearchView.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(74)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview()
+        }
+    }
 }
