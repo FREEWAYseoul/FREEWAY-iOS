86c0a644aa72fe4065c07ee5071af6eaa57f7e99
Taekhwan Han <hth3396@gmail.com>
2023-08-29 23:28:46 +0900

    feat: #1 현재위치 버튼 추가

---
+++ FREEWAY/FREEWAY/Presentation/MapsView/Components/CurrentLocationButton.swift
@@ -0,0 +1,45 @@
+//
+//  CurrentLocationButton.swift
+//  FREEWAY
+//
+//  Created by 한택환 on 2023/08/29.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class CurrentLocationButton: UIButton {
+    private let iconView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.image = UIImage(named: "currentLocationIcon")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension CurrentLocationButton {
+    func configure() {
+        self.addSubview(iconView)
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 6.67
+        self.layer.borderWidth = 0.83
+        self.layer.borderColor = UIColor.gray.cgColor
+    }
+    
+    func setupLayout() {
+        iconView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(16.67)
+        }
+        
+        iconView.adjustsImageSizeForAccessibilityContentSizeCategory = false
+    }
+}
