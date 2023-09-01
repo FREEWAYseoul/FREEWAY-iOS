607e286623890ab3c4c5b28a1ac5eb4bfb0836b4
Taekhwan Han <hth3396@gmail.com>
2023-09-01 22:29:53 +0900

    feat: #16 StationDetailCollectionView 구현

--- FREEWAY/FREEWAY/Presentation/StationDetailView/Components/StationDetailCollectionView.swift
+++ FREEWAY/FREEWAY/Presentation/StationDetailView/Components/StationDetailCollectionView.swift
@@ -5,4 +5,56 @@
 //  Created by 한택환 on 2023/08/30.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class StationDetailCollectionView: UIView {
+    
+    private let stationInfoItems: [(String, String)] = [("elevator", "엘리베이터"),("call", "안내전화"),("map", "역사지도"),("elevator", "편의시설")]
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = .zero
+        layout.minimumLineSpacing = .zero
+        layout.sectionInset = .zero
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        collectionView.register(StationDetailCollectionViewCell.self, forCellWithReuseIdentifier: StationDetailCollectionViewCell.stationDetailCollectionViewCellId)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+private extension StationDetailCollectionView {
+    func setupLayout() {
+        self.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension StationDetailCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stationInfoItems.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StationDetailCollectionViewCell.stationDetailCollectionViewCellId, for: indexPath) as? StationDetailCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.configure(stationInfoItems[indexPath.row].0, stationInfoItems[indexPath.row].1)
+        return cell
+    }
+}
--- FREEWAY/FREEWAY/Presentation/StationDetailView/Components/StationDetailCollectionViewCell.swift
+++ FREEWAY/FREEWAY/Presentation/StationDetailView/Components/StationDetailCollectionViewCell.swift
@@ -5,4 +5,76 @@
 //  Created by 한택환 on 2023/08/30.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class StationDetailCollectionViewCell: UICollectionViewCell {
+    
+    static let stationDetailCollectionViewCellId = "stationDetailCollectionViewCellId"
+    
+    var image: UIImage?
+    
+    private var iconView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private var iconLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = UIFont(name: "Pretendard-Regular", size: 14)
+        $0.layer.opacity = 0.5
+    }
+    
+    private let separator = UIView().then {
+        $0.backgroundColor = Pallete.dividerGray.color
+    }
+    
+    var selectStatus: Bool = false {
+        didSet {
+            self.iconView.tintColor = self.selectStatus ? Pallete.customBlue.color : .black
+            self.iconView.layer.opacity = self.selectStatus ? 1.0 : 0.5
+            
+            self.iconLabel.textColor = self.selectStatus ? Pallete.customBlue.color : .black
+            self.iconLabel.layer.opacity = self.selectStatus ? 1.0 : 0.5
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension StationDetailCollectionViewCell {
+    func configure(_ imageName: String, _ title: String) {
+        image = UIImage(named: imageName)
+        iconView.image = image
+        iconLabel.text = title
+    }
+    
+    private func setupLayout() {
+        self.addSubview(iconView)
+        iconView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.height.equalTo(28)
+        }
+        
+        self.addSubview(separator)
+        separator.snp.makeConstraints { make in
+            make.width.equalTo(1)
+            make.height.equalTo(35)
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(32)
+        }
+        
+        self.addSubview(iconLabel)
+        iconLabel.snp.makeConstraints { make in
+            make.top.equalTo(iconView).offset(16)
+            make.centerX.equalTo(iconView)
+        }
+    }
+}
