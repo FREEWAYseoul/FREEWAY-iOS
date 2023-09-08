d4356cd2b749130b66fa79d50609bd8869b7ab3c
Taekhwan Han <hth3396@gmail.com>
2023-09-09 02:25:47 +0900

    feat: #25 API 연동 구현

--- FREEWAY/FREEWAY/Services/NetworkService.swift
+++ FREEWAY/FREEWAY/Services/NetworkService.swift
@@ -9,5 +9,40 @@ import UIKit
 import Alamofire
 
 final class NetworkService {
+    static let shared = NetworkService()
     
+    private init() {
+        
+    }
+    
+
+    func getAllStationList(stationId: String = "", completion: @escaping ([StationDTO]?, Error?) -> Void) {
+        AF.request("http://freeway.ap-northeast-2.elasticbeanstalk.com/api/stations\(stationId)").responseDecodable(of: [StationDTO].self) { response in
+            switch response.result {
+            case .success(let stations):
+                // 성공적으로 디코딩된 데이터를 사용할 수 있습니다.
+                completion(stations, nil)
+            case .failure(let error):
+                // 데이터를 가져오는 중에 오류가 발생한 경우 처리할 내용을 작성합니다.
+                print("데이터를 불러오는 중 오류 발생: \(error)")
+                completion(nil, error)
+            }
+        }
+    }
+    
+    func getAlert() {
+        AF.request("https://freeway.ap-northeast-2.elasticbeanstalk.com/api/stations").responseJSON { response in
+            switch response.result {
+            case .success(let stations):
+                // 성공적으로 디코딩된 데이터를 사용할 수 있습니다.
+                print("불러온 역 목록: \(stations)")
+                
+                // 여기에서 데이터를 처리하거나 ViewModel 등으로 전달할 수 있습니다.
+            case .failure(let error):
+                // 데이터를 가져오는 중에 오류가 발생한 경우 처리할 내용을 작성합니다.
+                print("데이터를 불러오는 중 오류 발생: \(error)")
+            }
+        }
+    }
 }
+
