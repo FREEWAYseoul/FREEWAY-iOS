61ba8325c6e1eeb359fc94f4899c49eaa67bb937
Taekhwan Han <hth3396@gmail.com>
2023-09-13 00:37:31 +0900

    feat: #36 mail 기능 구현

--- FREEWAY/FREEWAY/Presentation/SettingView/SettingViewController.swift
+++ FREEWAY/FREEWAY/Presentation/SettingView/SettingViewController.swift
@@ -8,6 +8,7 @@
 import SnapKit
 import Then
 import UIKit
+import MessageUI
 
 final class SettingViewController: UIViewController {
     
@@ -48,6 +49,7 @@ final class SettingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        setDefaultNavigationBar()
         configure()
         setupLayout()
     }
@@ -99,7 +101,8 @@ extension SettingViewController: UITableViewDelegate {
             webViewCellPressed(webURL: "https://desert-enthusiasm-9d0.notion.site/077c4410b0964540b09bc735bdb93634?pvs=4")
         case .openSource:
             webViewCellPressed(webURL: "https://desert-enthusiasm-9d0.notion.site/2e50ba5f720b4d77a9a7c1e84b529867?pvs=4")
-        case .question: break
+        case .question:
+            touchUpInsideToMailToQuestionPage()
         }
     }
 }
@@ -124,5 +127,42 @@ private extension SettingViewController {
             make.top.equalTo(settingTitleView.snp.bottom)
             make.leading.bottom.trailing.equalToSuperview()
         }
+    }
+}
+
+extension SettingViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController,
+                               didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func touchUpInsideToMailToQuestionPage() {
+        if MFMailComposeViewController.canSendMail() {
+                let composeViewController = MFMailComposeViewController()
+                composeViewController.mailComposeDelegate = self
+                let bodyString = """
+                                 문의 내용을 입력해주세요
+                                 """
+                
+                composeViewController.setToRecipients([""])
+                composeViewController.setSubject("[문의]")
+                composeViewController.setMessageBody(bodyString, isHTML: false)
+                
+                self.present(composeViewController, animated: true, completion: nil)
+            } else {
+                print("메일 보내기 실패")
+                let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패", message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
+                let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+                    if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                }
+                let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+                
+                sendMailErrorAlert.addAction(goAppStoreAction)
+                sendMailErrorAlert.addAction(cancleAction)
+                self.present(sendMailErrorAlert, animated: true, completion: nil)
+            }
     }
 }
